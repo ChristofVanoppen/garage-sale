745a0807160509336f3ddad6be47c005bfdd6280
unknown <vanoppen.christof@hotmail.com>
2016-08-21 10:55:40 +0200

    register&login styled

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,17 +3,10 @@
 @section('content')
 
 
-<a class="login-logo" href="/"><img src="{{asset('images/logoo.png')}}" alt="Logo"></a>
-<div class="row register__photo">
-
-    <div class="col-md-4"></div>
-    <div class="col-xs-12 col-md-4 login__form__container ">
-        <div class="col-md-12 col-xs-12 login__container login__content">
-            <!--<h1>Registreer je nu en plaats gratis je opdrachten.</h1><br/><br/><h3>Kom snel en makkelijk in contact met de beste vakman voor jouw project.</h3>-->
-           <!-- <p class="login__social"><a href="#">Facebook</a><span class="login__circle">•</span><a href="#">Twitter</a><span class="login__circle">•</span><a href="#">Instagram</a></p>-->
-             <!-- <div class="col-md-3"></div>-->
-            <div class="col-md-12 col-xs-12 login__container login__form">
-            <h4>Registreer</h4><br>
+  <div class="wrapper">
+    <div class="form-signin">       
+      <h2 class="form-signin-heading">Please Register</h2>
+            
             
             @if(Session::has('success'))
                 <div class="alert alert-success">
@@ -42,13 +35,11 @@
                     <button class="btn btn-primary" type="submit">Registreer </button>
                 </div>
             {!! Form::close() !!}
-        </div>
-            <div class="col-md-3"></div>
-    </div>
-    </div>
+ </div>
+  </div>
     <div class="text-center signuplink">
         <span class="white-font">Je hebt al een account?</span> <a style="margin-left: 0px;" class="register" href="login">Login</a>
     </div>
-    <div class="col-md-4"></div>
+
 </div>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,13 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="{{asset('css/screen.css')}}">
 </head>
 <body>
 	@yield('content')
+
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 </html>
